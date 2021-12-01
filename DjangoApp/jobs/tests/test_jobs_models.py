b9884c5d612eefb17a3be554300d101c9edb2d18
claytoncook12 <claytoncook12@gmail.com>
2021-12-01 12:43:12 -0500

    add tunes app to webapp

---
+++ DjangoApp/jobs/tests/test_jobs_models.py
@@ -0,0 +1,46 @@
+# jobs\test\test_jobs_models.py
+
+from jobs.tests import factories
+
+import pytest
+
+@pytest.mark.django_db
+class TestCompanyFactory:
+    def test_init(self):
+        obj = factories.CompanyFactory()
+        assert obj.pk == 1, "Should save an instance"
+
+
+@pytest.mark.django_db
+class TestCurrentStatusFactory:
+    def test_init(self):
+        obj = factories.CurrentStatusFactory()
+        assert obj.pk == 1, "Should save an instance"
+
+
+@pytest.mark.django_db
+class TestPersonFactory:
+    def test_init(self):
+        obj = factories.PersonFactory()
+        assert obj.pk == 1, "Should save an instance"
+
+
+@pytest.mark.django_db
+class TestJobPostingFactory:
+    def test_init(self):
+        obj = factories.JobPostingFactory()
+        assert obj.pk == 1, "Should save an instance"
+
+
+@pytest.mark.django_db
+class TestPreInterviewFactory:
+    def test_init(self):
+        obj = factories.PreInterviewFactory()
+        assert obj.pk == 1, "Should save an instance"
+
+
+@pytest.mark.django_db
+class TestInterviewFactory:
+    def test_init(self):
+        obj = factories.InterviewFactory()
+        assert obj.pk == 1, "Should save an instance"
